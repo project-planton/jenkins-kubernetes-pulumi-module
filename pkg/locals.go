9e727542a14984432eb1781c66f3f38d43e60086
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-11-09 17:58:15 +0530

    switched over to using local generated stubs for project-planton apis

--- pkg/locals.go
+++ pkg/locals.go
@@ -1,10 +1,10 @@
 package pkg
 
 import (
-	jenkinskubernetesv1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/kubernetes/jenkinskubernetes/v1"
 	"fmt"
 	"github.com/project-planton/jenkins-kubernetes-pulumi-module/pkg/outputs"
-	"github.com/project-planton/pulumi-module-golang-commons/pkg/provider/kubernetes/kuberneteslabelkeys"
+	jenkinskubernetesv1 "github.com/project-planton/project-planton/apis/go/project/planton/provider/kubernetes/jenkinskubernetes/v1"
+	"github.com/project-planton/project-planton/pkg/pulmod/provider/kubernetes/kuberneteslabelkeys"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"strconv"
 )
--- pkg/main.go
+++ pkg/main.go
@@ -1,10 +1,10 @@
 package pkg
 
 import (
-	jenkinskubernetesv1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/kubernetes/jenkinskubernetes/v1"
 	"github.com/pkg/errors"
 	"github.com/project-planton/jenkins-kubernetes-pulumi-module/pkg/outputs"
-	"github.com/project-planton/pulumi-module-golang-commons/pkg/provider/kubernetes/pulumikubernetesprovider"
+	jenkinskubernetesv1 "github.com/project-planton/project-planton/apis/go/project/planton/provider/kubernetes/jenkinskubernetes/v1"
+	"github.com/project-planton/project-planton/pkg/pulmod/provider/kubernetes/pulumikubernetesprovider"
 	kubernetescorev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
